21598bb742d7800a81b896905fc5974bf7065746
urgentlion <urgentlioninfo@gmail.com>
2023-01-02 15:33:31 -0800

    ...

--- conditions.py
+++ conditions.py
@@ -1,3 +1,16 @@
 test1 = True == 1
-
 print(test1)
+test2 = False == 0
+print(test2)
+test3 = False == 1
+print(test3)
+test4 = True == 0
+print(test4)
+
+x = ""
+test5 = "need" < "ne" + x
+print(test5)
+
+x2 = "e"
+test6 = "need" < "ne" + x
+print(test6)
