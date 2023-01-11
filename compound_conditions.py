3b9ec6265f93a828d3d0653b6de4e543da9837f3
urgentlion <urgentlioninfo@gmail.com>
2023-01-10 18:20:44 -0800

    ...

--- compound_conditions.py
+++ compound_conditions.py
@@ -1,2 +1,17 @@
 result = not False and not True or True
 print(result)
+
+x = 0
+y = 0
+z = 0
+
+test_1 = x or y and z
+
+# T T T = T 
+# F T T = T
+# T F T = T
+# F F T = T
+# T T F = T
+# F T F = F
+# T F F = F
+# F F F = F
